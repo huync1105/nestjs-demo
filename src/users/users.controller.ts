964032b6aa292c0d897090a82f1a630f318a3eae
huync <huync1105@gmail.com>
2026-01-30 15:43:41 +0700

    New users api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as User from '../../generated/prisma/models/User';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: User.UserCreateInput) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.usersService.findAll();
   }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserCreateInput } from '../../generated/prisma/models/User';
+import { User } from '../../generated/prisma/client';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  @Inject() prisma: PrismaService;
+
+  async create(data: UserCreateInput): Promise<User> {
+    return this.prisma.user.create({ data });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
